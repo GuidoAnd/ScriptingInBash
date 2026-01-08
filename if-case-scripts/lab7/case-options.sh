68d63594e7863d867b8143e17e708d28cc451034
Guido <guido.andino@outlook.com>
2026-01-08 04:31:43 +0000

    Changing

--- if-case-scripts/lab7/case-options.sh
+++ if-case-scripts/lab7/case-options.sh
@@ -2,18 +2,26 @@
 
 helpf()
 {
-echo "Posibles opciones (Mostar fecha, usuarios conectados, carga del sistema y espacio utilizado)"
-echo "Debe escribir si o no"
+echo "Debe escribir si/s o no/n"
+sleep 1.5 
+exit
 }
+echo "Opciones disponibles (Mostar fecha, usuarios conectados, carga del sistema y espacio utilizado)"
+echo "..............................................................................................."
+echo "Debe escribir si/s o no/n"
+echo "........................."
 echo "¿Desea saber la Fecha?"
 read hola
 case "$hola" in
 si|s)
 echo "Hoy es $(date)"
+sleep 3
 ;;
 no|n)
 echo "Pasando a la siguiente opcion"
+sleep 0.5
 ;;
+*)helpf;;
 esac
 
 echo "¿Desea conocer los usuarios conectados?"
@@ -21,10 +29,20 @@ read hi
 case "$hi" in
 si|s)
 echo "Estos son los usarios conectados: $(users)"
-;;
-no|n)
+sleep 3
+echo "......................................"
+echo ".............................."
+echo "........................."
 echo "Pasando a la siguiente opcion"
 ;;
+no|n)
+echo "......................................"
+echo ".............................."
+echo "........................."
+echo "Pasando a la siguiente opcion"
+sleep 0.5
+;;
+*)helpf;;
 esac
 
 echo "¿Desea tener informacion sobre la carga del sistema?"
@@ -32,24 +50,40 @@ read nihao
 case "$nihao" in
 si|s)
 echo "Esta es la carga del sistema: $(sudo dmesg | head -n 22)"
-;;
-no|n)
+sleep 3
+echo "......................................"
+echo ".............................."
+echo "........................."
 echo "Pasando a la ultima opcion"
 ;;
+no|n)
+echo "......................................"
+echo ".............................."
+echo "........................."
+echo "Pasando a la ultima opcion"
+sleep 0.5
+;;
+*)helpf;;
 esac
 
 echo "¿Desea conocer el espacio disponible del dispositivo de almacenamiento principal?"
-read hola
-case "$hola" in
+read konichiwa
+case "$konichiwa" in
 si|s)
-echo "Este es el espacio disponible: $(df -h /)"
+echo "Este es el espacio disponible: "
+echo "$(df -h /)"
+sleep 3
 echo "......................................"
 echo ".............................."
 echo "........................."
 echo "Gracias por tu visita, vuelva pronto!"
 ;;
 no|n)
+echo "......................................"
+echo ".............................."
+echo "........................."
 echo "Gracias por tu visita, vuelva pronto!"
+sleep 0.1
 ;;
 *)helpf;;
 esac
